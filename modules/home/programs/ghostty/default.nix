630830c08da5b772a8f548cb6fe05b0273f59fc8
voidptrr <bruno.tommaso@protonmail.com>
2026-09-05 16:38:49 +0100

    chore: small changes on ghostty + update nvim

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -13,7 +13,9 @@
         command = "${pkgs.zsh}/bin/zsh -l";
         font-family = "JetBrains Mono";
         theme = "Black Metal (Gorgoroth)";
-        font-size = 13.5;
+        font-size = 12;
+        background-opacity = 0.95;
+        background-blur-radius = 20;
         window-decoration = "server";
         auto-update = "off";
       };
